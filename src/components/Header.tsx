839fe2574cb6717e5da4c7e58222ae0ddc4909bf
Namelessafk <marcusthuliok9@gmail.com>
2023-11-08 09:23:27 -0300

    Arrumando o Css do Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ class Header extends React.Component {
   render() {
     return (
       <div className="header">
-        <h1>Theory</h1>
+        <h2>Theory</h2>
         <p className="dia"> {new Date().toLocaleDateString()}</p>
       </div>
     );
