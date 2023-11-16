ceb11e7196cfd3362caf215563ed112f29af55d9
Namelessafk <marcusthuliok9@gmail.com>
2023-11-16 12:07:04 -0300

    Reajustando app e tirando main

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,33 @@
-import Header from "./components/Header";
-import Main from "./components/Main"
-import Scenario from "./components/Scenario";
+import { useContext } from "react";
+import { useState } from "react";
+
+
+import Header from "./components/Header"; //HEADER
+import Start from "./components/Start" // Pre-Game
+import Scenario from "./components/Scenario"; //Cenario
+import Profile from "./components/Profile";
+import Instructions from "./components/Instructions";
+import { Contextos } from './context/logica'
+
+
 
 function App() {
+  const [name, setName] = useState("");
+  const [contextoState, dispatch] = useContext(Contextos)
 
   return (
     <>
       <header>
         <Header />
       </header>
+
       <main>
-        <Main />
+        {contextoState.gameStage === "Start" && <Start setName={setName} />}
+        {contextoState.gameStage === "Start" && <Profile name={name} />}
+        {contextoState.gameStage === "Playing" && <Instructions />}
         <Scenario />
       </main>
+
     </>
   )
 }
